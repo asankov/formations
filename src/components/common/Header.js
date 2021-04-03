f37b8265088c9c150bf9fdd3bb654906ce542357
Anton Sankov <a.sankov96@gmail.com>
2021-04-04 02:10:40 +0300

    Add header
    - some refactoring
    - change font
    - remove some assets

---
+++ src/components/common/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span
+        style={{
+          height: "70px",
+          backgroundColor: "#370941",
+          width: "80%",
+          alignSelf: "flex-end",
+          marginBottom: "10px",
+        }}
+      >
+        <span
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            height: "100%",
+          }}
+        >
+          <span
+            style={{
+              color: "#ABACA7",
+              marginLeft: "15px",
+              lineHeight: "70px",
+              fontSize: "36px",
+              fontWeight: "bold",
+            }}
+          >
+            BUILD YOUR OWN PL TEAM
+          </span>
+          <span
+            style={{ color: "white", marginRight: "15px", lineHeight: "70px" }}
+          >
+            <span style={{ fontWeight: "100", cursor: "pointer" }}>
+              Sign up{" "}
+            </span>
+            |
+            <span style={{ cursor: "pointer", fontWeight: "bold" }}>
+              {" "}
+              Log In
+            </span>
+          </span>
+        </span>
+      </span>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/field/TeamName.js
+++ src/components/field/TeamName.js
@@ -1,13 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import plLogo from "../../images/logo.png";
 
 const TeamName = ({ teamName }) => (
   <div className="team-name-container">
     <span className="team-name">{teamName}</span>
-    <div className="pl-logo-container">
-      <img className="pl-logo" src={plLogo} alt="" />
-    </div>
   </div>
 );
 
